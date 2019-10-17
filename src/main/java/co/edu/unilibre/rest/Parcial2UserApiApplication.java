ed183a5a9bdfa656c59e3e41d459b310c8e1b1e7
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2019-10-16 20:10:44 -0500

    Creacion

---
+++ src/main/java/co/edu/unilibre/rest/Parcial2UserApiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unilibre.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcial2UserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parcial2UserApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unilibre/rest/Parcial2UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unilibre.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial2UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
